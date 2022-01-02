1179960b3f073bcbc3e56bf9669cb7f5b110cde2
김동현 <donghyeon@liveklass.com>
2022-01-02 15:00:22 +0900

    jpa case

--- src/main/java/com/jpa/jpatest/JpaTestApplication.java
+++ src/main/java/com/jpa/jpatest/JpaTestApplication.java
@@ -12,7 +12,6 @@ import org.springframework.context.annotation.Configuration;
 public class JpaTestApplication {
 
     public static void main(String[] args) {
-        
         SpringApplication app = new SpringApplication(JpaTestApplication.class);
         app.run(args);
     }
